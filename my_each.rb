e68fc87cc0af33092fa715c5602af3d55556c27e
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-18 02:16:15 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,13 +3,10 @@ def my_each (array)# put argument(s) here
   i = 0
   while i < array.length
     yield(array[i])
-    i += 1 
+    i += 1
   end
-  
-  
+
+
 end
 
-myeach(words) do |item|
-  item 
-  counter +=1
-end
+
